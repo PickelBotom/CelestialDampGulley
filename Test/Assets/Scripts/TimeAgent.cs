7f2fcd2e1bffa5b450c7a705faf45b45e085b77f
PickelBotom <s220610452@mandela.ac.za>
2024-10-03 22:48:07 +0200

    TimeAgent Bit
    DayNight Cycle
    Time agent
    Trees Drop Rocks
    -Every half day

---
+++ Test/Assets/Scripts/TimeAgent.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAgent : MonoBehaviour
+{
+    public Action onTimeTick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.TimeController.Sub(this);
+    }
+
+    public void Invoke()
+    {
+        onTimeTick?.Invoke();
+    } 
+    void OnDestroy()
+    {
+        GameManager.instance.TimeController.UnSub(this);
+    }
+}
